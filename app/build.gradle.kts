0e42e10b3af112d4b23231da56044b506238a0d5
shakthishamruth <shakthishamruth@gmail.com>
2025-07-24 00:29:37 +0530

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 2
-        versionName = "0.2"
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
